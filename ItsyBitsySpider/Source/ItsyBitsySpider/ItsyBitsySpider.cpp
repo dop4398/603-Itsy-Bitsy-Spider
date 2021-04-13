75f1b49656886af00842a9408392248a9c2c1437
David <dop4398@g.rit.edu>
2021-04-13 03:15:37 -0400

    ok

---
+++ ItsyBitsySpider/Source/ItsyBitsySpider/ItsyBitsySpider.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ItsyBitsySpider.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ItsyBitsySpider, "ItsyBitsySpider" );
